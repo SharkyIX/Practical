3aeb4437d5199003c92e9d748ed40fcba8e005e6
SharkyIX <89183770+SharkyIX@users.noreply.github.com>
2021-08-20 09:08:31 +0200

    minor changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace Practical
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Mitzu Marado!");
+            Console.WriteLine("My favorite play date is 12 august!");
+            Console.WriteLine("My best color is blue!");
+            Console.WriteLine("orange tastes bad, the color i mean!");
 
         }
     }
